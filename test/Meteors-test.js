ebcdb7c1da8528796e7c8a6832c08bc54c749ef8
Spencer Herms <slherms@gmail.com>
2017-10-10 14:01:54 -0600

    fixes Audio() error in all test files, adds framwork for all explosion testing

--- test/Meteors-test.js
+++ test/Meteors-test.js
@@ -1,16 +1,20 @@
 const { assert } = require('chai');
 
+global.Audio = class {};
+
 const Base = require('../lib/Base.js');
 const Cannon = require('../lib/Cannon.js');
 const Missile = require('../lib/Missile.js'); 
 const Meteors = require('../lib/Meteors.js');
-// const Explosion = require('../lib/Explosion.js'); 
+const Explosion = require('../lib/Explosion.js'); 
 
 describe('meteor unit testing', () => {
 
 	it('should be a function', () => {
-		let meteors = new Meteors();
+		const meteors = new Meteors();
 	});
+
+
 }); 
 
 // its should ...
