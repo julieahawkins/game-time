08b19652f1485f0678cdc3fd893447bd024d01bc
Spencer Herms <slherms@gmail.com>
2017-10-09 22:35:15 -0600

    adds test files for 5 objects

---
+++ test/MIssile-test.js
@@ -0,0 +1,32 @@
+const { assert } = require('chai');
+
+const Base = require('../lib/Base.js');
+const Cannon = require('../lib/Cannon.js');
+const Missile = require('../lib/Missile.js'); 
+const Meteors = require('../lib/Meteors.js');
+// const Explosion = require('../lib/Explosion.js'); 
+
+describe('missile unit testing', () => {
+
+	it('should be a function', () => {
+		let missile = new Missile();
+	});
+}); 
+
+// its should ...
+
+// x y w h 
+
+// dx = 0
+
+// dy = 0
+
+// target X Y undefined
+
+// isMoving hasArrived false 
+
+// draw()
+
+// move()
+
+// setVelocity
